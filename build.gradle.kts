cf92f581d27d7251fa0c33874854d7497cc49095
d1snin <me@d1s.dev>
2023-02-16 21:17:49 +0300

    refactor: include sources jar within the publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,8 @@ allprojects {
     version = projectVersion
 
     java {
+        withSourcesJar()
+
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
