292e07956493e4824a143852fecefc5366cf9731
d1snin <me@d1s.dev>
2023-02-19 22:07:53 +0300

    feat: implement `exkt-konform`

--- exkt-konform/README.md
+++ exkt-konform/README.md
@@ -1,7 +1,26 @@
 [![](https://maven.d1s.dev/api/badge/latest/releases/dev/d1s/exkt/exkt-konform?color=40c14a&name=maven.d1s.dev&prefix=v)](https://maven.d1s.dev/#/releases/dev/d1s/exkt/exkt-konform)
 
-### Exkt for Konform
+### Exkt Konform
 
-| ⚠️ Warning!                                                     |
-|-----------------------------------------------------------------|
-| **This module is currently WIP.** Thus, it's not available yet. |
+This Exkt module provides handy utilities for Konform validation framework. 
+It's mostly additional constraints.
+
+### Installation
+
+```kotlin
+repositories {
+    maven(url = "https://maven.d1s.dev/releases")
+}
+
+dependencies {
+    val exktVersion: String by project
+
+    implementation("dev.d1s.exkt:exkt-konform:$exktVersion")
+}
+```
+
+### Documentation
+
+| Module API                                 |
+|--------------------------------------------|
+| https://d1snin.github.io/exkt/exkt-konform |
---
+++ exkt-konform/src/main/kotlin/dev/d1s/exkt/konform/StringConstraints.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022-2023 Mikhail Titov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.d1s.exkt.konform
+
+import io.konform.validation.Constraint
+import io.konform.validation.ValidationBuilder
+import io.konform.validation.jsonschema.maxLength
+import io.konform.validation.jsonschema.minLength
+
+public fun ValidationBuilder<String>.isEmpty(): Constraint<String> = maxLength(0)
+
+public fun ValidationBuilder<String>.isNotEmpty(): Constraint<String> = minLength(1)
+
+public fun ValidationBuilder<String>.isBlank(): Constraint<String> = addConstraint("must be blank") {
+    it.isBlank()
+}
+
+public fun ValidationBuilder<String>.isNotBlank(): Constraint<String> = addConstraint("must not be blank") {
+    it.isNotBlank()
+}
