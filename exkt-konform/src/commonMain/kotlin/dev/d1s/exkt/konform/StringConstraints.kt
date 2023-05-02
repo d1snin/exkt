3edff4a61d5d1cd24a6e0017ffa07d4653ac0fc9
d1snin <me@d1s.dev>
2023-05-02 19:22:54 +0300

    feat(konform): add `ValidationBuilder<String>.matches`

--- exkt-konform/src/commonMain/kotlin/dev/d1s/exkt/konform/StringConstraints.kt
+++ exkt-konform/src/commonMain/kotlin/dev/d1s/exkt/konform/StringConstraints.kt
@@ -21,14 +21,23 @@ import io.konform.validation.ValidationBuilder
 import io.konform.validation.jsonschema.maxLength
 import io.konform.validation.jsonschema.minLength
 
-public fun ValidationBuilder<String>.isEmpty(): Constraint<String> = maxLength(0)
+public fun ValidationBuilder<String>.isEmpty(): Constraint<String> =
+    maxLength(0)
 
-public fun ValidationBuilder<String>.isNotEmpty(): Constraint<String> = minLength(1)
+public fun ValidationBuilder<String>.isNotEmpty(): Constraint<String> =
+    minLength(1)
 
-public fun ValidationBuilder<String>.isBlank(): Constraint<String> = addConstraint("must be blank") {
-    it.isBlank()
-}
+public fun ValidationBuilder<String>.isBlank(): Constraint<String> =
+    addConstraint("must be blank") {
+        it.isBlank()
+    }
 
-public fun ValidationBuilder<String>.isNotBlank(): Constraint<String> = addConstraint("must not be blank") {
-    it.isNotBlank()
-}
+public fun ValidationBuilder<String>.isNotBlank(): Constraint<String> =
+    addConstraint("must not be blank") {
+        it.isNotBlank()
+    }
+
+public fun ValidationBuilder<String>.matches(regex: Regex): Constraint<String> =
+    addConstraint("must match the expression: {0}", regex.toString()) {
+        it.matches(regex)
+    }
