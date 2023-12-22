2931f4a6924402f9a3e2b6faf30e7339a3e4ea8d
d1snin <me@d1s.dev>
2023-12-22 03:10:35 +0300

    feat: add icon utils

---
+++ exkt-kvision/src/jsMain/kotlin/dev/d1s/exkt/kvision/bootstrap/BootstrapIcon.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022-2023 Mikhail Titov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.d1s.exkt.kvision.bootstrap
+
+import dev.d1s.exkt.kvision.iconWithMargin
+import io.kvision.html.Icon
+import io.kvision.html.icon
+import io.kvision.panel.SimplePanel
+
+private const val BOOTSTRAP_ICON_PREFIX = "bi bi-"
+
+public fun SimplePanel.bootstrapIcon(icon: String): Icon =
+    icon("$BOOTSTRAP_ICON_PREFIX$icon")
+
+public fun SimplePanel.bootstrapIconWithMargin(icon: String, margin: Int = 2): Icon =
+    iconWithMargin("$BOOTSTRAP_ICON_PREFIX$icon", margin)
